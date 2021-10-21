e8e527464d212cc5b6bac9f1a8486e547131f9f6
Eneda Xhambazi <enedaxhambazi@gmail.com>
2021-10-20 22:36:07 -0400

    refactored data routes and views

--- controllers/data.js
+++ controllers/data.js
@@ -4,11 +4,22 @@ const User = require("../models/user");
 const {getToday} = require("../utils/getToday");
 
 
-module.exports.renderData = async (req, res) => {
+module.exports.renderTodayData = async (req, res) => {
     const user = await User.findById(req.user.id)
     const escapedCountry = escape(user.country.name);
     const today = getToday();
     const longToday = new Date().toLocaleDateString( 'en-US', {year: 'numeric', month: 'long', day: 'numeric'});
-    res.render("data/data", {countries, user, escapedCountry, today, longToday, title:"Data / todei"})
+    res.render("data/today", {countries, user, escapedCountry, today, longToday, title:"Data - Today / todei"})
 }
 
+module.exports.renderAllData = async (req, res) => {
+    const user = await User.findById(req.user.id)
+    const escapedCountry = escape(user.country.name);
+    res.render("data/all", {countries, user, escapedCountry, title:"Data - All / todei"})
+}
+
+module.exports.renderMyData = async (req, res) => {
+    const user = await User.findById(req.user.id)
+    const escapedCountry = escape(user.country.name);
+    res.render("data/me", {countries, user, escapedCountry, today, longToday, title:"Data - Me / todei"})
+}
--- routes/data.js
+++ routes/data.js
@@ -5,6 +5,12 @@ const data = require("../controllers/data");
 const {isLoggedIn, setPostedToday, blockDuplicatePost, checkPostStreak, isAccountOwner} = require("../middleware");
 
 router.route("/")
-        .get( isLoggedIn, setPostedToday, catchAsync(data.renderData));
+        .get( isLoggedIn, setPostedToday, catchAsync(data.renderTodayData));
+
+router.route("/all")
+        .get( isLoggedIn, setPostedToday, catchAsync(data.renderAllData));
+        
+router.route("/me")
+        .get( isLoggedIn, setPostedToday, catchAsync(data.renderMyData));     
 
 module.exports = router;
