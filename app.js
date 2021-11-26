b16a3f240d161542afb18e42bdfb80fa27e021bc
Eneda Xhambazi <enedaxhambazi@gmail.com>
2021-11-26 17:28:35 -0500

    more https forwarding

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ if(process.env.NODE_ENV == "production") {
     app.get('*', function(req, res, next) {
             if (req.get('x-forwarded-proto') != "https") {
                 res.set('x-forwarded-proto', 'https');
-                res.redirect('https://' + req.get('host') + req.url);
+                res.redirect('https://www.' + req.headers.host + req.url);
             } else {
                 next();     
             }
