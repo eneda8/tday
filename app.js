d5786cefa74edd77f6e7be086e6b9e8ce6987d09
Eneda Xhambazi <enedaxhambazi@gmail.com>
2021-12-04 13:11:39 -0500

    debugging heroku

--- app.js
+++ app.js
@@ -220,8 +220,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error", {err, title: "Error / t'day"});
 })
 
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(3000, () => {
-  console.log(`Serving on port 3000`)
+  console.log(`Serving on port ${port}`)
 })
