0f8663d83609dd5185317ee679010b8a04128097
Eneda Xhambazi <enedaxhambazi@gmail.com>
2021-12-07 21:03:06 -0500

    refactored timezone scripts, added timezone cookie, updated password requirements

--- app.js
+++ app.js
@@ -116,6 +116,19 @@ const sessionConfig = {
     }
 }
 
+app.use((req, res, next) => {
+    const { headers: { cookie } } = req;
+    if (cookie) {
+        const values = cookie.split(';').reduce((res, item) => {
+            const [name, value] = item.trim().split('=');
+            return { ...res, [name]: value };
+        }, {});
+        res.locals.cookie = values;
+    }
+    else res.locals.cookie = {};
+    next();
+});
+
 app.use(session(sessionConfig))
 app.use(flash());
 
--- public/javascripts/time.js
+++ public/javascripts/time.js
@@ -11,5 +11,25 @@
         const editedAt = stamp.getAttribute("title");
         stamp.setAttribute("title", new Date(editedAt).toLocaleString())
     }
+
+    if(document.title.includes("Register") || document.title.includes("Login")){
+        const timezone = new Date().toLocaleTimeString('en-us',{timeZoneName:'short'}).split(' ')[2];
+        document.getElementById("tz").value = timezone; // set user timezone in the database
+        document.cookie = `timezone=${timezone}`; // set user timezone in cookie
+        console.log("TIME ZONE SET TO:", document.getElementById("tz").value); // for transparency purposes
+    }
+    if(document.getElementById("today-header")){
+        document.getElementById("today-header").innerText = new Date().toLocaleDateString( 'en-US', {year: 'numeric', month: 'long', day: 'numeric'})
+    }
+    if(document.getElementById("dayOfTheWeek")){
+        document.getElementById("dayOfTheWeek").textContent = new Date().toLocaleString('en-us', {weekday:'long'});
+    }
+    if(document.querySelector("#postDate")){
+        document.querySelector("#postDate").value = new Date().toLocaleDateString('en-US',{year: 'numeric', month: 'short',day: 'numeric'});
+    }
+    if(document.getElementById("journalDate")){
+        document.getElementById("journalDate").value = new Date().toLocaleString();
+        document.getElementById("journalLabel").innerText = new Date().toLocaleString();
+    }
 })()
 
